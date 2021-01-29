4c3225d9d7c76602a5bdb253cbfc41c19a5473cd
Zhermit09 <71440273+Zhermit09@users.noreply.github.com>
2021-01-29 01:08:32 +0100

    wall detection at y-axis (in progress)

--- script.js
+++ script.js
@@ -1,81 +1,199 @@
+//@ts-check
+
 let c = document.getElementById("myCanvas");
 let ctx = c.getContext("2d");
 
 ctx.canvas.width = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
 
+const times = [];
+let fps;
+
 let px = 100;
 let py = 100;
-let pa = Math.PI;
-let pDx = Math.cos(pa) * 5;
-let pDy = Math.sin(pa) * 5;
+let pa = 1.5* Math.PI;
+let pDx = Math.cos(pa);
+let pDy = Math.sin(pa);
+
+function refreshLoop() {
+  window.requestAnimationFrame(() => {
+    const now = performance.now();
+    while (times.length > 0 && times[0] <= now - 1000) {
+      times.shift();
+    }
+    times.push(now);
+    fps = times.length;
+    refreshLoop();
+  });
+}
+
+refreshLoop();
+
+function animate() {
+  var request = requestAnimationFrame(animate);
+  console.log(" hitX = " + rayX + " hitY = " + rayY);
+  draw();
+}
 
 document.addEventListener("keydown", function (event) {
   if (event.keyCode == 37) {
-    rotation(-0.15);
-    drawPlayer();
+    rotation(-Math.PI / 100);
+    draw();
   } else if (event.keyCode == 38) {
     px += pDx;
     py += pDy;
-    drawPlayer();
+    draw();
   } else if (event.keyCode == 39) {
-    rotation(0.15);
-    drawPlayer();
+    rotation(Math.PI / 100);
+    draw();
   } else if (event.keyCode == 40) {
     px -= pDx;
     py -= pDy;
-    drawPlayer();
+    draw();
   }
 });
 
-function drawPlayer() {
+let temp;
+
+function draw() {
   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
   mapDraw();
   drawRay();
+  ctx.fillStyle = "black";
+  ctx.fillRect(px - 7, py - 7, 13, 13);
   ctx.fillStyle = "yellow";
-  ctx.fillRect(px, py, 11, 11);
+  ctx.fillRect(px - 6, py - 6, 11, 11);
+  ctx.font = "17px Arial";
+  ctx.fillText("FPS: " + fps, ctx.canvas.width - 70, 25);
+  ray();
+
   // console.log(Math.PI);
 }
 
 let mapX = 8,
   mapY = 8,
-  mapS = 63;
+  mapS = 64;
 let map = [
-  [1, 1, 1, 1, 1, 1, 1, 1],
-  [1, 0, 1, 0, 0, 0, 0, 1],
-  [1, 0, 1, 0, 0, 0, 0, 1],
-  [1, 0, 0, 0, 1, 1, 0, 1],
-  [1, 0, 0, 0, 0, 1, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 1],
-  [1, 1, 1, 1, 1, 1, 1, 1],
+  ["@", "@", "@", "@", "@", "@", "@", "@"],
+  ["@", "-", "@", "-", "-", "-", "-", "@"],
+  ["@", "-", "@", "-", "-", "-", "-", "@"],
+  ["@", "-", "-", "-", "@", "@", "-", "@"],
+  ["@", "-", "-", "-", "-", "@", "-", "@"],
+  ["@", "-", "-", "-", "-", "-", "-", "@"],
+  ["@", "-", "-", "-", "-", "-", "-", "@"],
+  ["@", "@", "@", "@", "@", "@", "@", "@"],
 ];
 
 function drawRay() {
-  ctx.strokeStyle = "red";
+  ctx.strokeStyle = "green";
   ctx.beginPath();
-  ctx.moveTo(px + 6, py + 6);
-  ctx.lineTo(px + pDx * 15 + 6, py + pDy * 15 + 6);
+  ctx.moveTo(px, py);
+  ctx.lineTo(px + pDx * 100, py + pDy * 100);
   ctx.stroke();
 }
 
 function mapDraw() {
   ctx.fillStyle = "black";
-  for (i = 0; i < mapY; i++) {
-    for (j = 0; j < mapX; j++) {
-      if (map[i][j] == 1) {
+  for (var i = 0; i < mapY; i++) {
+    for (var j = 0; j < mapX; j++) {
+      if (map[i][j] == "@") {
         ctx.fillStyle = "white";
-      } else {
+      } else if (map[i][j] == "-") {
         ctx.fillStyle = "black";
       }
-      ctx.fillRect(j * mapS + j, i * mapS + i, mapS, mapS);
+      ctx.fillRect(j * (mapS - 1) + j, i * (mapS - 1) + i, mapS - 1, mapS - 1);
     }
   }
 }
 
 function rotation(rot) {
   pa += rot;
+  if (pa >= 2 * Math.PI) {
+    pa -= 2 * Math.PI;
+  } else if (pa <= 0) {
+    pa += 2 * Math.PI;
+  }
+  pDy = Math.sin(pa);
+  pDx = Math.cos(pa);
+
   console.log(pa);
-  pDy = Math.sin(pa) * 5;
-  pDx = Math.cos(pa) * 5;
+}
+
+let ra = pa;
+let diffY;
+let rayY;
+let rayX;
+let oY;
+let oX;
+let mrX = Math.floor(rayX / mapS);
+let mrY = rayY / mapS - 1;
+let dof;
+
+function ray() {
+  diffY = py % mapS;
+  dof = 0;
+  
+
+  if (pa > Math.PI) {
+    rayY = py - diffY;
+    rayX = (-1 / Math.tan(pa)) * (py - rayY) + px;
+    oY = -mapS;
+  } else if (pa < Math.PI) {
+    rayY = py + (mapS - diffY);
+    rayX = (1 / Math.tan(pa)) * (rayY - py) + px;
+    oY = mapS;
+  } else if (pa == 0 || pa == Math.PI) {
+    rayX = px;
+    rayY = py;
+    dof = 8;
+  }
+
+  if (0.5 * Math.PI < pa && 1.5 * Math.PI) {
+    oX = -mapS;
+  } else {
+    oX = mapS;
+  }
+
+  //console.log("Map x = " + rayX + " Map y = " + rayY);
+
+  //console.log("pdx = " + pDx + " pdy = " + pDy);
+
+  console.log(
+    "Map x = " +
+      mrX +
+      " \nMap y = " +
+      mrY +
+      "\nWall yes/no: " +
+      map[mrY][mrX] +
+      "\n pa =" +
+      pa
+  );
+
+  console.log("Tile bef: " + map[mrY][mrX]);
+  for (let i = dof; i < 8; i++) {
+    console.log("Tile in: " + map[mrY][mrX]);
+    if (map[mrY][mrX] == "-") {
+      rayX += 0;
+      rayY += -64;
+      mrX = Math.floor(rayX / mapS);
+      mrY = rayY / mapS - 1;
+      i++;
+      console.log("Im here");
+    } else {
+      console.log("Tile af: " + map[mrY][mrX]);
+      i = 8;
+
+      console.log("ray stopped");
+      //  mrX = Math.floor(rayX / mapS);
+      // mrY = rayY / mapS - 1;
+      map[mrY][mrX];
+    }
+  }
+  ctx.strokeStyle = "red";
+  ctx.beginPath();
+  ctx.moveTo(px, py);
+  ctx.lineTo(rayX, rayY);
+  ctx.stroke();
+
+  // console.log("Ray x: " + rayX, "Ray y: " + rayY);
 }
